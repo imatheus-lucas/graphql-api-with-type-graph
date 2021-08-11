5c94e0b8a1fa58081940c3b60ca56662a5544e12
imatheus-lucas <matheeuslucas13@gmail.com>
2021-08-11 10:39:13 -0300

    add commenters

--- src/modules/resolvers/videoResolvers.ts
+++ src/modules/resolvers/videoResolvers.ts
@@ -17,12 +17,21 @@ class VideoInput {
 
 @Resolver()
 export class VideoResolver {
+  /*O type Mutation serve para definir o contrato de
+   manipulação de dados. A funcionalidade dele é comparável com a 
+   dos verbos POST, PUT, PATCH e DELETE de APIs REST.
+   */
+
   @Mutation(() => Video)
   async addVideo(@Arg("videoInput") videoInput: VideoInput) {
     const video = await videoSchema.create(videoInput);
     return video;
   }
 
+  /*
+  O type Query serve para definir o contrato de consulta de dados. 
+  A funcionalidade dele é comparável com o verbo GET de APIs REST. 
+  */
   @Query(() => [Video])
   async videos() {
     const videos = await videoSchema.find();
